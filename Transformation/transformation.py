2a72f8380da9ebda873001b675694c06d89d7030
HUSTHY <840499869@qq.com>
2019-08-11 12:36:02 +0800

    转化操作实例  tarnsformation

---
+++ Transformation/transformation.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+'''
+@Author  :    HY
+@Software:   PyCharm
+@File    :   transformation.py
+@Time    :   2019/8/11 11:33
+@Desc    :
+
+'''
+
+from pyspark import SparkConf,SparkContext
+
+def MAP(rdd):#操作每个元素
+    r=rdd.map(lambda x:int(x)*int(x))#这里读出来的就是str 要做类型转化
+    print(r.collect())
+def Filter(rdd):#过滤
+    r=rdd.filter(lambda x:int(x)>2)
+    print(r.collect())
+
+def MapFlat(rdd):
+    r=rdd.flatMap(lambda line:line.strip().split(' '))#每一行是一个str然后str用空格分开
+    print(r.collect())
+
+def distinct(rdd):#去重
+    r=rdd.distinct()
+    print(r.collect())
+    print(r.count())
+
+def differenceofMAPandFlatMap(rdd):#map和flatMap的不同
+    r1=rdd.map(lambda line:line.strip().split(' '))
+    r2=rdd.flatMap(lambda line:line.strip().split(' '))
+    print(r1.count())
+    print(r1.collect())
+    print(r2.count())
+    print(r2.collect())
+
+if __name__ == '__main__':
+    conf = SparkConf().setMaster("local").setAppName("Transformaion")
+    sc=SparkContext(conf=conf)
+    rdd=sc.textFile('data.txt')
+    # MAP(rdd)
+    # Filter(rdd)
+    strrdd=sc.textFile('str.txt')
+    # MapFlat(strrdd)
+    differenceofMAPandFlatMap(strrdd)
+    # distinct(rdd)
